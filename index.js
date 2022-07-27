0588e2b2cf1558ea9022951fe347d53d00c5c7b0
Gum Ng <jon9609@gmail.com>
2022-07-27 11:36:34 -0700

    added variable to change images based on random number generated and added query selector to first images to use the new variable

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 let randomNumber1 = Math.floor((Math.random() * 6) + 1);
 
+let images1 = "images/dice"+ randomNumber1 +".png";
+
+document.querySelector(`.dice .img1`).setAttribute(`src`, images1);
