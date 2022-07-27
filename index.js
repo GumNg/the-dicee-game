b78cd5199cea10ca661533c6dce0dbbf33b9cdcb
Gum Ng <jon9609@gmail.com>
2022-07-27 12:00:22 -0700

    added second random number generator and img selector for dice 2 and updated names

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-// random number generator
+// random number generator for dice1
 let randomNumber1 = Math.floor((Math.random() * 6) + 1);
 
+// random number generator for dice2
+let randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
 // change img based on random number generator for first dice image
-let randomImg = "images/dice"+ randomNumber1 +".png";
+let randomImg1 = "images/dice"+ randomNumber1 +".png";
+
+// change img based on random number generator for 2nd dice image
+let randomImg2 = "images/dice"+ randomNumber2 +".png";
 
 // select first dice img and change img src 
-document.querySelector(`.dice .img1`).setAttribute(`src`, randomImg);
+document.querySelector(`.dice .img1`).setAttribute(`src`, randomImg1);
 
 // select second dice img and change img src
-document.querySelector(`.dice .img2`).setAttribute(`src`, randomImg);
+document.querySelector(`.dice .img2`).setAttribute(`src`, randomImg2);
 
