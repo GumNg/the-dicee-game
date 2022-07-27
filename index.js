6b780172bd56c89d3dcca3f43287ffd08bea0127
Gum Ng <jon9609@gmail.com>
2022-07-27 11:51:07 -0700

    changed variable name and updated variable inside setAttribute

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 let randomNumber1 = Math.floor((Math.random() * 6) + 1);
 
 // change img based on random number generator for first dice image
-let images1 = "images/dice"+ randomNumber1 +".png";
+let randomImg = "images/dice"+ randomNumber1 +".png";
 
 // select first dice img and change img src 
-document.querySelector(`.dice .img1`).setAttribute(`src`, images1);
+document.querySelector(`.dice .img1`).setAttribute(`src`, randomImg);
+
+document.querySelector(`.dice .img2`).setAttribute(`src`, randomImg);
+
